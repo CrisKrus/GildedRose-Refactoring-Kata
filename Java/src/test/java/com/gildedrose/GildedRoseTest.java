510a08e762770f44f53f1008ca730ab0d74e6ba1
Cristian Suarez <cristian.suarez@leanmind.es>
2024-05-30 14:42:46 +0100

    use combination approval instead

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,76 +1,25 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] {
-            new Item("foo", 0, 0),
-            new Item("foo", 5, 0),
-            new Item("foo", 10, 0),
-            new Item("foo", 0, 5),
-            new Item("foo", 5, 5),
-            new Item("foo", 10, 5),
-        };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        Approvals.verifyAll("foo", Arrays.asList(items));
+    void updateItemQuality() {
+        CombinationApprovals.verifyAllCombinations(
+            (name, sellIn, quality) -> updateQuality(name, sellIn, quality).toString().trim(),
+            new String[]{"foo", "Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"}, // name
+            new Integer[]{0, 5, 10, 100}, // sellIn
+            new Integer[]{0, 5, 10, 50} // quality
+        );
     }
 
-    @Test
-    void AgedBrie() {
-        Item[] items = new Item[] {
-            new Item("Aged Brie", 0, 0),
-            new Item("Aged Brie", 5, 0),
-            new Item("Aged Brie", 10, 0),
-            new Item("Aged Brie", 0, 5),
-            new Item("Aged Brie", 5, 5),
-            new Item("Aged Brie", 10, 5),
-        };
-        GildedRose app = new GildedRose(items);
+    private static Item updateQuality(String name, int sellIn, int quality) {
+        Item item = new Item(name, sellIn, quality);
+        GildedRose app = new GildedRose(new Item[] { item });
         app.updateQuality();
-
-        Approvals.verifyAll("Aged Brie", Arrays.asList(items));
-    }
-
-    @Test
-    void SulfurasHadnOfRagnaros() {
-        Item[] items = new Item[] {
-            new Item("Sulfuras, Hand of Ragnaros", 0, 0),
-            new Item("Sulfuras, Hand of Ragnaros", 5, 0),
-            new Item("Sulfuras, Hand of Ragnaros", 10, 0),
-            new Item("Sulfuras, Hand of Ragnaros", 0, 5),
-            new Item("Sulfuras, Hand of Ragnaros", 5, 5),
-            new Item("Sulfuras, Hand of Ragnaros", 10, 5),
-        };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        Approvals.verifyAll("Sulturas, Hand of Ragnaros", Arrays.asList(items));
-    }
-
-    @Test
-    void BackstagePassesToATafkal80etcConcert() {
-        Item[] items = new Item[] {
-            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5),
-        };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        Approvals.verifyAll("Backstage passes", Arrays.asList(items));
+        return item;
     }
 
 }
